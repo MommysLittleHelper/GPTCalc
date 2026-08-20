bbbaf3b90bd1e9577fb4223ba471032e06e6ba50
MommysLittleHelper <artem.railship@gmail.com>
2026-08-20 17:09:04 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'calc-volume-v1.0.9';
+const CACHE = 'calc-volume-v1.1.1';
 const ASSETS = ['./','./index.html','./script.js','./style.css','./manifest.json'];
 
 self.addEventListener('install', event => {
